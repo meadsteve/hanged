47d6a7d6f2623218667ae943f0a54aa6dac4dca4
Steve B <steve@meadsteve.dev>
2022-06-22 14:50:25 +0200

    Defer construction of concrete types
    In your code the concrete classes matching those interfaces gets constructed when this module is imported. If you switch to singleton definitions they will only be loaded when needed.

--- services/application/containers.py
+++ services/application/containers.py
@@ -12,6 +12,6 @@ from services.application.ports.outgoing.i_user_output_port import IUserOutputPo
 from services.application.ports.outgoing.user_output_port import UserOutputPort
 
 container = Container()
-container[IUserAdapter] = container[DynamoDBUserAdapter]
-container[IUserOutputPort] = container[UserOutputPort]
-container[IUserInputPort] = container[ApiGatewayUserInputPort]
+container[IUserAdapter] = Singleton(DynamoDBUserAdapter)
+container[IUserOutputPort] = Singleton(UserOutputPort)
+container[IUserInputPort] = Singleton(ApiGatewayUserInputPort)
